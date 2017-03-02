0f93c65f51b19f0c4ead4401a8c3b12154c68b95
Matt Light <matt.light@lightdatasys.com>
2017-03-01 23:48:42 -0800

    Add weekly recurring payroll dates
    Issue #1

---
+++ test/payroll-dates.js
@@ -0,0 +1,49 @@
+var requirejs = require('requirejs');
+var test = require('tape');
+
+requirejs.config({
+  baseUrl: 'src',
+});
+
+test('calculate every 2 weeks next payroll dates', function(t) {
+  t.plan(1);
+
+  var payrollDates = requirejs('payroll-dates');
+  var paySchedule = payrollDates({
+    repeats: 'weekly',
+    repeatEvery: 2,
+    startsOn: '2016-10-19',
+  });
+
+  t.deepEqual(
+    paySchedule.next(4, '2017-02-20'),
+    [
+      (new Date('2017-02-22')),
+      (new Date('2017-03-08')),
+      (new Date('2017-03-22')),
+      (new Date('2017-04-05')),
+    ]
+  );
+});
+
+test('calculate every 2 weeks next historical payroll dates', function(t) {
+  t.plan(1);
+
+  var payrollDates = requirejs('payroll-dates');
+  var paySchedule = payrollDates({
+    repeats: 'weekly',
+    repeatEvery: 2,
+    startsOn: '2016-10-19',
+  });
+
+  t.deepEqual(
+    paySchedule.next(5, '2016-04-04'),
+    [
+      (new Date('2016-04-06')),
+      (new Date('2016-04-20')),
+      (new Date('2016-05-04')),
+      (new Date('2016-05-18')),
+      (new Date('2016-06-01')),
+    ]
+  );
+});
